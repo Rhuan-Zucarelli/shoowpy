ac2f7249b84e1d262e8f2359cdf1535499add5c7
Rhuan-Zucarelli <rhuan1zucarelli@gmail.com>
2023-04-11 00:05:57 -0300

    trabalho chines 2

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import {
-    LeftContainer,
-    NavbarContainer,
-    NavbarExtendContainer,
-    NavbarLink,
-    NavbarLinkExtend,
-    NavInnerContainer,
-    OpenLinkButton,
-    RightContainer
+	LeftContainer,
+	NavbarContainer,
+	NavbarExtendContainer,
+	NavbarLink,
+	NavbarLinkExtend,
+	NavInnerContainer,
+	OpenLinkButton,
+	RightContainer
 } from './styles'
 
 import { Carrinho } from '../../pages/Carrinho'
@@ -16,91 +16,91 @@ import { Carrinho } from '../../pages/Carrinho'
 // https://github.com/profchines
 
 interface interfData {
-    id: number,
-    categoria: string
-    url : string
+	id: number,
+	categoria: string
+	url : string
 }
 
 export const Menu = () => {
-    const [extendNavbar, setExtendNavBar] = useState(false);
-    const [dataMenu, setMenu] = useState<Array<interfData>>([]);
+	const [extendNavbar, setExtendNavBar] = useState(false);
+	const [dataMenu, setMenu] = useState<Array<interfData>>([]);
 
-    useEffect(() => {
+	useEffect(() => {
 
-        axios.get('http://localhost:3001/categorias')
-            .then((response) => {
-                setMenu(response.data)
-            })
-            .catch((erro) => {
-                console.log(erro)
-            })
+		axios.get('http://localhost:3001/categorias')
+			.then((response) => {
+				setMenu(response.data)
+			})
+			.catch((erro) => {
+				console.log(erro)
+			})
 
-    }, [])
+	}, [])
 
-    return (
-        <NavbarContainer
-            extendNavbar={extendNavbar}
-        >
-            <NavInnerContainer>
-                <LeftContainer>
+	return (
+		<NavbarContainer
+			extendNavbar={extendNavbar}
+		>
+			<NavInnerContainer>
+				<LeftContainer>
 
-                    <OpenLinkButton
-                        onClick={() => {
+					<OpenLinkButton
+						onClick={() => {
 
-                            setExtendNavBar((atual) => !atual)
+							setExtendNavBar((atual) => !atual)
 
-                        }}
-                    >
-                        {extendNavbar ? <>&#10005;</> : <>&#8801;</>}
-                    </OpenLinkButton>
+						}}
+					>
+						{extendNavbar ? <>&#10005;</> : <>&#8801;</>}
+					</OpenLinkButton>
 
-                    <NavbarLinkExtend
-                        to='/'
-                    >
-                        Shoowpy
-                    </NavbarLinkExtend>
+					<NavbarLinkExtend
+						to='/'
+					>
+						Shoowpy
+					</NavbarLinkExtend>
 
-                    <NavbarLink to='/'>Home</NavbarLink>
-                    {
-                        dataMenu.map((menu) => (
-                            <NavbarLink
-                                key={menu.id}
-                                to={menu.url}
-                            >
-                                {
-                                    menu.categoria
-                                }
-                            </NavbarLink>
-                        ))
-                    }
+					<NavbarLink to='/'>Home</NavbarLink>
+					{
+						dataMenu.map((menu) => (
+							<NavbarLink
+								key={menu.id}
+								to={menu.url}
+							>
+								{
+									menu.categoria
+								}
+							</NavbarLink>
+						))
+					}
 
-                </LeftContainer>
-                <RightContainer>
-                    <NavbarLinkExtend to='Carrinho' >
-                        Carrinho
-                    </NavbarLinkExtend>
-                </RightContainer>
-            </NavInnerContainer>
+				</LeftContainer>
+				<RightContainer>
+					<NavbarLinkExtend to='/carrinho' >
+						Carrinho
+					</NavbarLinkExtend>
+				</RightContainer>
+			</NavInnerContainer>
 
-            {
-                extendNavbar && (
-                    <NavbarExtendContainer>
-                        {
-                            dataMenu.map((menu) => (
-                                <NavbarLinkExtend
-                                    key={menu.id}
-                                    to={menu.url}
-                                >
-                                    {
-                                        menu.categoria
-                                    }
-                                </NavbarLinkExtend>
-                            ))
-                        }
-                    </NavbarExtendContainer>
-                )
-            }
+			{
+				extendNavbar && (
+					<NavbarExtendContainer>
+						{
+							dataMenu.map((menu) => (
+								<NavbarLinkExtend
+									key={menu.id}
+									to={menu.url}
+								>
+									{
+										menu.categoria
+									}
+								</NavbarLinkExtend>
+							))
+						}
+					</NavbarExtendContainer>
+				)
+			}
 
-        </NavbarContainer>
-    )
+		</NavbarContainer>
+	)
 }
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -4,175 +4,175 @@ import { FaTrash } from 'react-icons/fa'
 import { Button, TBTr, THTh, THtr, Table, Td, TextButton } from './styles'
 
 interface interfProdutos {
-	id_categoria: number
-	total: number
+  id_categoria: number
+  total: number
 
-	id: string
-	imagemg: string
-	imagemp: string
-	nome: string
-	promo: string
-	promoNumber: string
-	quantidade: string
-	valor: string
+  id: string
+  imagemg: string
+  imagemp: string
+  nome: string
+  promo: string
+  promoNumber: string
+  quantidade: string
+  valor: string
 }
 
 export const Carrinho = () => {
 
-	const [dataCarrinho, setDataCarrinho] = useState<Array<interfProdutos>>([])
-	const [valorTotal, setValorTotal] = useState<number>(0)
+  const [dataCarrinho, setDataCarrinho] = useState<Array<interfProdutos>>([])
+  const [valorTotal, setValorTotal] = useState<number>(0)
 
-	const atualizaValorTotal = useCallback(
-		(carrinho: Array<interfProdutos>) => {
-			let total = 0
+  const atualizaValorTotal = useCallback(
+    (carrinho: Array<interfProdutos>) => {
+      let total = 0
 
-			carrinho.forEach((produto) => {
-				total = produto.total + total
-			})
-			setValorTotal(total)
+      carrinho.forEach((produto) => {
+        total = produto.total + total
+      })
+      setValorTotal(total)
 
-		}, [])
-	// function atualizaValorTotal(
-	//     carrinho: Array<interfProdutos>
-	// ) {
-	//     let total = 0
+    }, [])
+  // function atualizaValorTotal(
+  //     carrinho: Array<interfProdutos>
+  // ) {
+  //     let total = 0
 
 
-	//     carrinho.forEach((produto) => {
-	//         total = produto.total + total
-	//     })
-	//     setValorTotal(total)
+  //     carrinho.forEach((produto) => {
+  //         total = produto.total + total
+  //     })
+  //     setValorTotal(total)
 
-	//     console.log(valorTotal)
+  //     console.log(valorTotal)
 
-	// }
+  // }
 
-	useEffect(() => {
-		let lsCarrinho = localStorage.getItem(
-			'@shoowpy:carrinho'
-		)
-		let carrinho: any = null
+  useEffect(() => {
+    let lsCarrinho = localStorage.getItem(
+      '@shoowpy:carrinho'
+    )
+    let carrinho: any = null
 
-		if (typeof lsCarrinho === 'string') {
-			carrinho = JSON.parse(lsCarrinho)
-		}
+    if (typeof lsCarrinho === 'string') {
+      carrinho = JSON.parse(lsCarrinho)
+    }
 
-		if (carrinho) {
-			setDataCarrinho(carrinho)
-			atualizaValorTotal(carrinho)
-		}
+    if (carrinho) {
+      setDataCarrinho(carrinho)
+      atualizaValorTotal(carrinho)
+    }
 
-	}, [])
+  }, [])
 
-	function removerProdutoCarrinho(id: string) {
-		let carrinho = dataCarrinho.filter((produto) => (
-			produto.id !== id
-		))
+  function removerProdutoCarrinho(id: string) {
+    let carrinho = dataCarrinho.filter((produto) => (
+      produto.id !== id
+    ))
 
-		localStorage.setItem(
-			'@shoowpy:carrinho',
-			JSON.stringify(carrinho)
-		)
+    localStorage.setItem(
+      '@shoowpy:carrinho',
+      JSON.stringify(carrinho)
+    )
 
-		setDataCarrinho(carrinho)
-		atualizaValorTotal(carrinho)
-	}
-	const limparCarrinho = () => {
-		localStorage.removeItem(
-			'@shoowpy:carrinho'
-		)
-		setDataCarrinho([])
-	}
+    setDataCarrinho(carrinho)
+    atualizaValorTotal(carrinho)
+  }
+  const limparCarrinho = () => {
+    localStorage.removeItem(
+      '@shoowpy:carrinho'
+    )
+    setDataCarrinho([])
+  }
 
-	return (
-		<>
-			<Menu />
-			<div
-				style={{
-					paddingLeft: '6%',
-					paddingRight: '6%',
-					marginTop: 20,
-					marginBottom: 40
-				}}
-			>
-				<h2>Carrinho de compras</h2>
+  return (
+    <>
+      <Menu />
+      <div
+        style={{
+          paddingLeft: '6%',
+          paddingRight: '6%',
+          marginTop: 20,
+          marginBottom: 40
+        }}
+      >
+        <h2>Carrinho de compras</h2>
 
-				<Table>
-					<thead>
-						<THtr>
-							<THTh
-								style={{
-									minWidth: 300
-								}}
-							>Nome do Produto</THTh>
-							<THTh>Quantidade</THTh>
-							<THTh>Vlr Unit.</THTh>
-							<THTh>Vlr Total.</THTh>
-							<THTh>Ações</THTh>
-						</THtr>
-					</thead>
-					<tbody>
+        <Table>
+          <thead>
+            <THtr>
+              <THTh
+                style={{
+                  minWidth: 300
+                }}
+              >Nome do Produto</THTh>
+              <THTh>Quantidade</THTh>
+              <THTh>Vlr Unit.</THTh>
+              <THTh>Vlr Total.</THTh>
+              <THTh>Ações</THTh>
+            </THtr>
+          </thead>
+          <tbody>
 
-						{
-							dataCarrinho.map((produto) => {
-								return (
-									<TBTr key={produto.id}>
-										<Td width={300}>{produto.nome}</Td>
-										<Td>{produto.quantidade}</Td>
-										<Td>{produto.promo}</Td>
-										<Td>{produto.total}</Td>
-										<Td>
-											<Button
-												type='button'
-												onClick={() => {
-													removerProdutoCarrinho(produto.id)
-												}}
-											>
-												<TextButton>
-													<FaTrash />
-												</TextButton>
-											</Button>
-										</Td>
-									</TBTr>
-								)
-							})
-						}
+            {
+              dataCarrinho.map((produto) => {
+                return (
+                  <TBTr key={produto.id}>
+                    <Td width={300}>{produto.nome}</Td>
+                    <Td>{produto.quantidade}</Td>
+                    <Td>{produto.promo}</Td>
+                    <Td>{produto.total}</Td>
+                    <Td>
+                      <Button
+                        type='button'
+                        onClick={() => {
+                          removerProdutoCarrinho(produto.id)
+                        }}
+                      >
+                        <TextButton>
+                          <FaTrash />
+                        </TextButton>
+                      </Button>
+                    </Td>
+                  </TBTr>
+                )
+              })
+            }
 
-					</tbody>
-					<tfoot>
-						<TBTr>
-							<Td width={300}>Valor Total</Td>
-							<Td></Td>
-							<Td></Td>
-							<Td>{valorTotal}</Td>
-							<Td></Td>
-						</TBTr>
-					</tfoot>
-				</Table>
+          </tbody>
+          <tfoot>
+            <TBTr>
+              <Td width={300}>Valor Total</Td>
+              <Td></Td>
+              <Td></Td>
+              <Td>{valorTotal}</Td>
+              <Td></Td>
+            </TBTr>
+          </tfoot>
+        </Table>
 
-				{
-					dataCarrinho.length > 0 &&
-					<div
-						style={{
-							display: 'flex',
-							justifyContent: 'space-between'
-						}}
-					>
-						<Button
-							type='button'
-							onClick={limparCarrinho}
-						>
-							<TextButton>Limpar carrinho</TextButton>
-						</Button>
-						<Button
-							type='button'
-							bgColor='green'
-						>
-							<TextButton>Finalizar pedido</TextButton>
-						</Button>
-					</div>
-				}
-			</div>
-		</>
-	)
+        {
+          dataCarrinho.length > 0 &&
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between'
+            }}
+          >
+            <Button
+              type='button'
+              onClick={limparCarrinho}
+            >
+              <TextButton>Limpar carrinho</TextButton>
+            </Button>
+            <Button
+              type='button'
+              bgColor='green'
+            >
+              <TextButton>Finalizar pedido</TextButton>
+            </Button>
+          </div>
+        }
+      </div>
+    </>
+  )
 }
